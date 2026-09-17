4a8d3b8158835e2a547d597170467e1bbcab6ed1
Joshua Lin <Joshua.lin@ku.edu>
2026-09-17 15:10:23 -0500

    Harder restrictions and redesign

--- Minesweeper.py
+++ Minesweeper.py
@@ -3,25 +3,30 @@
 #PLEASE FEEL FREE TO MAKE EDITS JUST INSURE THAT YOU CHANGE OTHER CORRESPONDING CALLS
 class Minesweeper:
     def __init__(self, x, y, n):
+
+        #THIS IS THE AUTHORITATIVE LOCATION OF ALL THE BOMBS
+        #THIS SHOULD BE TREATED AS STATIC UNLESS STARTING
         self.matrix = self.MineAlgorithm(x, y, n)
         #Board sizes
         self.x_size = 10
         self.y_size = 10
-        #Array to track where RecOpen has been
-        self.visited = [[0] * self.y_size for _ in range(self.x_size)]
+        
+        #THIS WILL BE THE VISIBLE PART OF THE ARRAY HANDED UP TO THE USER
+        #THIS SHOULD BE THE ONLY THING THAT IS EDITED THROUGHOUT THE PROGRAM
+        self.visited = [None * self.y_size for _ in range(self.x_size)]
 
     def RecOpen(self, x, y):
         #Check if the coords passed in are invalid 
         if x < 0 or y < 0 or x >= self.x_size or y >= self.y_size:
             return
         #Check if we have already checked this square
-        if self.visited[x][y] > 0:
+        if self.visited[x][y] is not None:
             return
 
         #Check for mines in near by squares
         adjacentMines = self.CheckSquare(x,y)
         #Set this tile to be visited
-        self.visited[x][y] = 1
+        self.visited[x][y] = adjacentMines
         #We only want to reveal tiles if this square has no mines near it
         if adjacentMines == 0:
             #Update the matrix to show that this square is uncovered
@@ -38,9 +43,6 @@ class Minesweeper:
             self.RecOpen(x-1,y+1) #Top left diagonal
             self.RecOpen(x+1,y-1) #Bottom right diagonal
             self.RecOpen(x-1,y-1) #Bottom left diagonal
-        else:
-            #Update to indicate there are nearby mines
-            self.matrix[x][y] = 2
         return
 
     def CheckSquare(self, x, y):
@@ -59,6 +61,7 @@ class Minesweeper:
     #UI should only call outcome and flag, use self.matrix
     def Outcome(self, x, y):
         #this should determine game outcome/win/loss
+        #SHOULD RETURN NUMERICAL VALUE TO MATCH
         pass
 
     def Flag(self, x, y):
--- main.py
+++ main.py
@@ -25,22 +25,27 @@ class setupRequest(BaseModel):
 def index():
     return FileResponse("frontend/index.html")
 
-# endpoint to retrieve the board state if needed
+# endpoint to retrieve the board state, this is the only way for the code to get board state
+# THIS RETURNS NUMERIC VALUES FOR EVERY SQUARE
+# 0 MEAN IT HAS BEEN VISITED AND NIL / NONE MEANS IT HASN"T
+# OTHER NUMBERS REPRESENTE ADJACENT BOMB COUNT
 @app.get("/board")
 def getState():
     if game:
-        return { "board": game.matrix }
+        return { "board": game.visited }
     else:
         raise HTTPException(status_code=404, detail="Board not found")
 
 # endpoint that takes the board configuration values and initializes the game state
+## THIS NEEDS TO BE REMOVED OR MOVED TO BASIC CLICK/BOARD ENDPOINT
 @app.post("/setup")
 def setupGame(setup: setupRequest):
     global game
 
     game = Minesweeper(setup.x, setup.y, setup.n)
-    return { "board": game.matrix }
 
+
+####THESE FUNCTIONS SHOULDN"T RETURN BOARD & OUTCOME/BOARD UPDATE IS BROKEN
 # endpoint that provides functionality for updating the board state
 @app.post("/board")
 def boardUpdate(click: clickRequest):
