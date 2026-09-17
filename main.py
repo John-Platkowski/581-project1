1bab080edd7825fb0f58b2c7fbeb69a529679558
JackLarson-KU <jacklarson214@ku.edu>
2026-09-17 14:35:45 -0500

    added incredibly basic and open cors policy, someone more knowledgable on web dev should check this

--- main.py
+++ main.py
@@ -4,8 +4,22 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from Minesweeper import Minesweeper
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+
 game = None
 
 # pydantic schemas
