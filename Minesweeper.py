c6db236598081f3147ed14cbe5039c8b80a079b3
Joshua Lin <Joshua.lin@ku.edu>
2026-09-20 01:01:32 -0500

    Fixed error due to empty board + flagging. Updated backend to refuse flag creation if there are already too many

--- Minesweeper.py
+++ Minesweeper.py
@@ -172,6 +172,9 @@ class Minesweeper:
 
         #Can return number of placed flags to make displaying easier
     def Flag(self, x, y):
+        #if no available flags, don't do anything
+        if self.RemainingMines() <= 0:
+            return
         #Places a flag on the square if it's uncovered (flag identifier is -1 since other positive numbers represent number of mines)
         if (self.visited[x][y] is UNVISITED):
             self.visited[x][y] = FLAG
