a3ed686230a16f9ba74741f988b8a487242cee4e
Trey Timko <179626011+TTimCo3@users.noreply.github.com>
2026-09-20 14:13:50 -0500

    fixed up comments for my code and got rid of unused get endpoint

--- main.py
+++ main.py
@@ -32,68 +32,83 @@ app.add_middleware(
 game = None
 mine_count = 10
 
+# Trey - 09/17/26
 # pydantic schemas
+# this ensures the data from the frontend is modeled correctly
 class clickRequest(BaseModel):
+    # clickRequest is the model of a request sent upon every user click
+    
+    # it holds the x value of the click
     x: int
+    # as well as the y
     y: int
 
 class setupRequest(BaseModel):
+    # setup request is the model of a request that needs a new board setup
+
+    # it takes the desired minecount of the user
     n: int
-    
+
 # fastapi endpoints to be used
 
+# Trey - 09/17/26
 # endpoint to provide the frontend
 @app.get("/")
 def index():
+    # when accessing the "/" endpoint of the server index.html is returned
+    # as to be rendered in the users browser
     return FileResponse("frontend/index.html")
 
-# endpoint to retrieve the board state, this is the only way for the code to get board state
-# THIS RETURNS NUMERIC VALUES FOR EVERY SQUARE
-# 0 MEAN IT HAS BEEN VISITED AND NIL / NONE MEANS IT HASN"T
-# OTHER NUMBERS REPRESENTE ADJACENT BOMB COUNT
-@app.get("/board")
-def getState():
-    if game:
-        return { "board": game.visited }
-    else:
-        raise HTTPException(status_code=404, detail="Board not found")
-
-# endpoint that takes the board configuration values and initializes the game state
-## THIS NEEDS TO BE REMOVED OR MOVED TO BASIC CLICK/BOARD ENDPOINT
+# Trey - 09/17/26 and 09/18/26
+# endpoint that takes the mine configuration value and resets the game state to prepare for a new game
 @app.post("/setup")
 def setupGame(setup: setupRequest):
+    # because this file keeps track of the users specified mine_count
+    # and the game state, the global variables are used in this function
     global mine_count
     global game
-
+    
+    # the mine_count given is assigned
     mine_count = setup.n
+    # and the game state is reverted to None to prepare for a new game
     game = None
 
-####THESE FUNCTIONS SHOULDN"T RETURN BOARD & OUTCOME/BOARD UPDATE IS BROKEN
-# endpoint that provides functionality for updating the board state
+# Trey - 09/17/26 and 09/18/26
+# endpoint that takes the x and y information of a click, handles the initialization of the game
+# game state if neccessary, passes of the information as to determine the outcome of the game
+# and hands back an updated board, the result of the game, and the remaining mines.
 @app.post("/board")
 def boardUpdate(click: clickRequest):
+    # the global variable tracked across the file for the game state
     global game
 
+    # if the game is not initialized
     if game is None:
+        # initialized it using the mine_count determined in setup, and provide the x and y 
+        # cordinates for the cell clicked
         game = Minesweeper(click.x,click.y,mine_count)
-    
+
+    # pass of the information about the cell click and store the information about the result
     result = game.Outcome(click.x, click.y)
 
     return {
-            "board": game.visited,
-            "result": result,
-            "mines": game.RemainingMines()
+            "board": game.visited, # return the board that was updated by backend processes
+            "result": result, # return the retrieved result
+            "mines": game.RemainingMines() # return the count of remaining mines
             }
 
-
-# endpoint that handles requests requiring flagging functionality
+# Trey - 09/17/26
+# endpoint that takes x and y information of a click, returns an updated board after the placed flag
+# and the remaining number of mines
 @app.post("/flag")
 def flagCell(click: clickRequest):
+    # if the game exists
     if game:
+        # then pass the click information to the Flag function
         game.Flag(click.x, click.y)
-        return { 
-            "board": game.visited,
-            "mines": game.RemainingMines()
+        return {
+            "board": game.visited, # return the updated board state
+            "mines": game.RemainingMines() # and the count of remaining mines
             }
     else:
         raise HTTPException(status_code=404, detail="Board not found")
